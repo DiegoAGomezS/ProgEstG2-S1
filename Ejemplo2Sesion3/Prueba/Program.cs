48bffc4f0445915fcb9ae89f52b636facdefe0fd
DiegoAGomezS <dagomez@uamv.edu.ni>
2025-08-29 11:45:18 -0600

    E1S1

--- Ejemplo2Sesion3/Prueba/Program.cs
+++ Ejemplo2Sesion3/Prueba/Program.cs
@@ -4,24 +4,19 @@ class Program
 {
     static void Main()
     {
-        // 1. Declarar variables
         double longitud;
-        double ancho = 5; // Ancho fijo en varas
+        double ancho = 5;
         double areaVaras;
         double areaManzanas;
-        double factorConversion = 1428.8; // 1 manzana = 1428.8 varas²
+        double factorConversion = 1428.8;
 
-        // 2. Pedir al usuario la longitud del terreno
         Console.Write("Ingrese la longitud del terreno en varas: ");
         longitud = Convert.ToDouble(Console.ReadLine());
 
-        // 3. Calcular área en varas²
         areaVaras = longitud * ancho;
 
-        // 4. Convertir a manzanas
         areaManzanas = areaVaras / factorConversion;
 
-        // 5. Mostrar resultado
         Console.WriteLine("El área del terreno es: " + areaManzanas + " manzanas.");
     }
 }
