e9c2a8949dd601d78dbe44b9bcda7c324a3e7c43
DiegoAGomezS <dagomez@uamv.edu.ni>
2025-08-29 11:55:25 -0600

    E3S1

---
+++ Ej3S1/Prueba/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main()
+    {
+        double velocidad;
+        double tiempo;
+        double distancia;
+
+        Console.Write("Ingrese la velocidad promedio en km/h: ");
+        velocidad = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el tiempo recorrido en horas: ");
+        tiempo = Convert.ToDouble(Console.ReadLine());
+
+        distancia = velocidad * tiempo;
+
+        Console.WriteLine("La distancia total recorrida es: " + distancia + " km.");
+    }
+}
+
