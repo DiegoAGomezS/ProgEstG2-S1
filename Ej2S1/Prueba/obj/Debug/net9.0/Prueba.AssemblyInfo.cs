10af245b5cf15d8f171c076920f54c36f786c100
DiegoAGomezS <dagomez@uamv.edu.ni>
2025-08-29 11:44:51 -0600

    E1S1

--- Ej2S1/Prueba/obj/Debug/net9.0/Prueba.AssemblyInfo.cs
+++ Ej2S1/Prueba/obj/Debug/net9.0/Prueba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1b77d38800accb0638e7327eb516fae8f6be1b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
